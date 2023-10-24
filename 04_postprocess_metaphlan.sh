cf7ba489c041b193c8f79937f7ebf95e5cb961fa
Svenja Schorlemmer <116006468+schorlsa@users.noreply.github.com>
2023-10-24 13:50:14 +0200

    Update 04_postprocess_metaphlan.sh

--- 04_postprocess_metaphlan.sh
+++ 04_postprocess_metaphlan.sh
@@ -1,6 +1,7 @@
 #process results from metaphlan run
 
 #singularity shell --bind /nfs/data/projects/PCOS_Qi_2019/analysis/metaphlan4/ /nfs/data/TOOLs/singularity/images/metaphlan4.sif
+#singularity exec --bind /nfs/data/projects/healthy_enable_2023 /nfs/data/TOOLs/singularity/images/metaphlan4.sif bash /nfs/data/projects/healthy_enable_2023/mgx-reference/04_postprocess_metaphlan.sh
 
 #save programme version
 output_file="progr_info.txt"
@@ -16,25 +17,27 @@ else
     echo "Failed to retrieve the version information."
 fi
 
+prj_name=healthy_enable_2023
+metaphlan_root=/nfs/data/projects/${prj_name}/analysis/metaphlan4
 
 #fix header problem
-find ../profile/ -name "*.profile.txt" -type f | xargs sed -i -e '/reads processed/d'
-find ../profile/ -name "*.profile.txt" -type f | xargs sed -i -e '/estimated_reads/d'
+find ${metaphlan_root}/profile/ -name "*.profile.txt" -type f | xargs sed -i -e '/reads processed/d'
+find ${metaphlan_root}/profile/ -name "*.profile.txt" -type f | xargs sed -i -e '/estimated_reads/d'
 
 #generate table of merged abundances from all samples
-merge_metaphlan_tables.py ../profile/*.profile.txt > merged_abundance_table.txt
+merge_metaphlan_tables.py ${metaphlan_root}/profile/*.profile.txt > ${metaphlan_root}/analysis/merged_abundance_table.txt
 
 #prepare for visualisation
 #grep -E "s__|clade" merged_abundance_table.txt | grep -E -v "t__" merged_abundance_table.txt | sed 's/^.*s__//g'| sed -e 's/clade_name/bacterial_species/g' > test_merged_abundance_table_species.txt
 
 #species level
-grep -E "s__|profile" merged_abundance_table.txt | grep -E -v "t__" | sed 's/^.*s__//g'| cut -f1,3-42 | sed -e 's/clade_name/bacterial_species/g' > merged_abundance_table_species.txt
+grep -E "s__|profile" ${metaphlan_root}/analysis/merged_abundance_table.txt | grep -E -v "t__" | sed 's/^.*s__//g'| cut -f1,3-42 | sed -e 's/clade_name/bacterial_species/g' > ${metaphlan_root}/analysis/merged_abundance_table_species.txt
 
 #phylum level
-grep -E "p__|profile" merged_abundance_table.txt | grep -E -v "c__" | sed 's/^.*p__//g'| cut -f1,3-42 | sed -e 's/clade_name/bacterial_phylum/g' > merged_abundance_table_phylum.txt
+grep -E "p__|profile" ${metaphlan_root}/analysis/merged_abundance_table.txt | grep -E -v "c__" | sed 's/^.*p__//g'| cut -f1,3-42 | sed -e 's/clade_name/bacterial_phylum/g' > ${metaphlan_root}/analysis/merged_abundance_table_phylum.txt
 
 #phylum level
-grep -E "g__|profile" merged_abundance_table.txt | grep -E -v "s__" | sed 's/^.*g__//g'| cut -f1,3-42 | sed -e 's/clade_name/bacterial_genus/g' > merged_abundance_table_genus.txt
+grep -E "g__|profile" ${metaphlan_root}/analysis/merged_abundance_table.txt | grep -E -v "s__" | sed 's/^.*g__//g'| cut -f1,3-42 | sed -e 's/clade_name/bacterial_genus/g' > ${metaphlan_root}/analysis/merged_abundance_table_genus.txt
 
 #grep: search for expression s__ or clade
 #only extract species name, cut off the rest (replace by nothing)
