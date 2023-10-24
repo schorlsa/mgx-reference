771942dbfe8e69bafbbd8d413dda9bd3119d3fd2
Svenja Schorlemmer <116006468+schorlsa@users.noreply.github.com>
2023-10-24 13:21:14 +0200

    automate for different cohorts 01_prepare_enter_docker.sh

--- 01_prepare_enter_docker.sh
+++ 01_prepare_enter_docker.sh
@@ -1,9 +1,18 @@
-prj_name=PCOS_Qi_2019
+#!/bin/bash
+
+#run with bash 01_prepare_enter_docker.sh healthy_enable_2023 (or any other project name)
+
+#cwd=$(pwd)
+#prj_name=$(echo ${cwd} | cut -d'/' -f5)
+
+#prj_name=healthy_enable_2023
+prj_name=$1
 R1_suffix=_R1.fq.gz
 R2_suffix=_R2.fq.gz
 
 mgx_cleaned_dir=/nfs/data/projects/${prj_name}/QC/mgx_cleaned
 metaphlan4_root=/nfs/data/projects/${prj_name}/analysis/metaphlan4
+#metaphlan4_root=${cwd}
 metaphlan4_DB_dir=/nfs/data/database/metaphlan4_DB
 
 profile_dir=${metaphlan4_root}/profile
@@ -14,7 +23,5 @@ mkdir -p ${profile_dir}
 mkdir -p ${sam_dir}
 mkdir -p ${analysis_dir}
 
-#docker run --group-add 1002 -v /nfs:/nfs -it biobakery/metaphlan
-docker run --user "$(id -u):$(id -g)" -v /nfs:/nfs -it biobakery/metaphlan
-
-
+#docker run --user "$(id -u):$(id -g)" -v /nfs:/nfs -it biobakery/metaphlan bash /nfs/data/projects/${prj_name}/mgx-reference/02_metaphlan.sh
+docker run --user "$(id -u):$(id -g)" -v /nfs:/nfs -it biobakery/metaphlan bash ~/mgx-reference/02_metaphlan.sh $prj_name
